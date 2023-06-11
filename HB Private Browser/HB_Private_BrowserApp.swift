fdbbd854e60db0943c8db59a6b2efecefbc608c1
Benjamin Prentiss <bschenk18@yahoo.com>
2023-06-11 01:40:40 -0700

    Initial Commit

---
+++ HB Private Browser/HB_Private_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  HB_Private_BrowserApp.swift
+//  HB Private Browser
+//
+//  Created by Benjamin Prentiss on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HB_Private_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
