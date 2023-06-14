97687c51b7a5301f79eae9fbc7829632713d2338
Benjamin Prentiss <bschenk18@yahoo.com>
2023-06-14 00:43:58 -0700

    tracking protection

--- HB Private Browser/WebViewState.swift
+++ HB Private Browser/WebViewState.swift
@@ -5,27 +5,40 @@ import WebKit
 class WebViewState: ObservableObject {
     @Published var urlToLoad: URL?
     @Published var searchText = ""
-    public var webView: WKWebView = WKWebView()
-
+    public var webView: WKWebView?
+    
+    private var trackingPreventionConfiguration: WKWebViewConfiguration {
+        let configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = WKWebsiteDataStore.nonPersistent()
+        configuration.preferences = WKPreferences()
+        
+        let webpagePreferences = WKWebpagePreferences()
+        webpagePreferences.allowsContentJavaScript = false
+        configuration.defaultWebpagePreferences = webpagePreferences
+        
+        return configuration
+    }
+    
     func handleSearch(searchText: String) {
         guard let encodedSearch = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
               let url = URL(string: "https://duckduckgo.com/?q=\(encodedSearch)") else { return }
         urlToLoad = url
     }
-
+    
     func refresh() {
-        webView.reload()
+        webView?.reload()
     }
-
+    
     func goBack() {
-        if webView.canGoBack {
-            webView.goBack()
-        }
+        webView?.goBack()
     }
-
+    
     func goForward() {
-        if webView.canGoForward {
-            webView.goForward()
-        }
+        webView?.goForward()
+    }
+    
+    func setupWebView() {
+        let configuration = trackingPreventionConfiguration
+        webView = WKWebView(frame: .zero, configuration: configuration)
     }
 }
