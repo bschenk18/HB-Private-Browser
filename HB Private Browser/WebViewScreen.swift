ab6b1a57ccaaeb0a553d75ab3c128547af668137
Benjamin Prentiss <bschenk18@yahoo.com>
2023-06-14 02:45:52 -0700

    hyperbold

--- HB Private Browser/WebViewScreen.swift
+++ HB Private Browser/WebViewScreen.swift
@@ -2,94 +2,92 @@ import SwiftUI
 import WebKit
 
 struct WebViewScreen: View {
-   @ObservedObject var webViewState: WebViewState
-   @State private var showNewWebViewScreen = false
-    @Binding var showWebView: Bool // <- Here is the new binding
+    @ObservedObject var webViewState: WebViewState
+    @Binding var showWebView: Bool
 
-   var body: some View {
-       VStack {
-           HStack {
-               
-               // Home Button
-                              Button(action: {
-                                  self.showWebView = false // <- Dismiss the WebViewScreen
-                              }) {
-                                  Image(systemName: "house")
-                                      .foregroundColor(.blue)
-                              }
-                              .padding(.trailing, 6)
-               
-               TextField("", text: $webViewState.searchText, onCommit: {
-                   webViewState.handleSearch(searchText: webViewState.searchText)
-               })
-               .padding(7)
-               .padding(.horizontal, 25)
-               .background(Color(.systemGray6))
-               .cornerRadius(8)
-               .overlay(
-                   HStack {
-                       Image(systemName: "magnifyingglass")
-                           .foregroundColor(.gray)
-                           .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                           .padding(.leading, 8)
-                       
-                       Button(action: {
-                           webViewState.refresh()
-                       }) {
-                           Image(systemName: "arrow.clockwise")
-                               .foregroundColor(.blue)
-                               .padding(.trailing, 16)
-                       }
-                   }
-               )
-               .frame(maxWidth: .infinity, alignment: .leading)
-
-           }
-           .padding(.horizontal, 10)
-
-           Spacer()
-
-           if let urlToLoad = webViewState.urlToLoad {
-               WebViewWrapper(url: urlToLoad, webViewState: webViewState)
-           }
-
-           Spacer()
-
-           HStack {
-               Button(action: {
-                   webViewState.goBack()
-               }) {
-                   Image(systemName: "arrow.left")
-               }
-
-               Spacer()
-               
-               Button(action: {
-                   webViewState.goForward()
-               }) {
-                   Image(systemName: "arrow.right")
-               }
-
-               Spacer()
-               
-               // Use your image here
-               Image(systemName: "bold")
-                   .foregroundColor(.primary)
-           }
-           .padding(.horizontal, 20)
-           .padding(.vertical, 10)  // Add vertical padding here
-           .frame(maxWidth: .infinity)
-           .background(Color(.systemGray5))
-       }
-       .navigationBarHidden(true)
-       .onTapGesture {
-           hideKeyboard()
-       }
-       .environment(\.colorScheme, .light) // Always light mode for WebViewScreen
-   }
-
-   func hideKeyboard() {
-       UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-   }
-   
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    self.showWebView = false
+                }) {
+                    Image(systemName: "house")
+                        .foregroundColor(.blue)
+                }
+                .padding(.trailing, 6)
+                
+                TextField("", text: $webViewState.searchText, onCommit: {
+                    webViewState.handleSearch(searchText: webViewState.searchText)
+                })
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        Button(action: {
+                            webViewState.refresh()
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                                .foregroundColor(.blue)
+                                .padding(.trailing, 16)
+                        }
+                    }
+                )
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.horizontal, 10)
+            
+            Spacer()
+            
+            if let urlToLoad = webViewState.urlToLoad {
+                WebViewWrapper(url: urlToLoad, webViewState: webViewState)
+            }
+            
+            Spacer()
+            
+            HStack {
+                Button(action: {
+                    webViewState.goBack()
+                }) {
+                    Image(systemName: "arrow.left")
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    webViewState.goForward()
+                }) {
+                    Image(systemName: "arrow.right")
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    webViewState.toggleHyperBold()
+                }) {
+                    Image(systemName: "bold")
+                }
+                .foregroundColor(webViewState.isHyperBoldEnabled ? .blue : .primary)
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 10)
+            .frame(maxWidth: .infinity)
+            .background(Color(.systemGray5))
+        }
+        .navigationBarHidden(true)
+        .onTapGesture {
+            hideKeyboard()
+        }
+        .environment(\.colorScheme, .light)
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
--- HB Private Browser/WebViewState.swift
+++ HB Private Browser/WebViewState.swift
@@ -8,110 +8,137 @@ class WebViewState: ObservableObject {
     @Published var isIncognitoModeOn = true
     @Published var isDNTEnabled = true
     @Published var isUserAgentSpoofingEnabled = true
-    @Published var isAdBlockEnabled = true // New property for ad-blocking
-    
+    @Published var isHyperBoldEnabled = false
+
     public var webView: WKWebView?
-    
+
     private var configuration: WKWebViewConfiguration {
         let configuration = WKWebViewConfiguration()
         configuration.websiteDataStore = isIncognitoModeOn ? WKWebsiteDataStore.nonPersistent() : WKWebsiteDataStore.default()
         configuration.preferences = WKPreferences()
-        
+
         let webpagePreferences = WKWebpagePreferences()
         webpagePreferences.allowsContentJavaScript = !isIncognitoModeOn
         configuration.defaultWebpagePreferences = webpagePreferences
-        
+
         // Configure Do Not Track (DNT)
         if isDNTEnabled {
             enableDNT()
         } else {
             disableDNT()
         }
-        
+
         // Configure User Agent Spoofing
         if isUserAgentSpoofingEnabled {
             configureUserAgentSpoofing()
         }
-        
-        // Configure Ad Blocking
-        if isAdBlockEnabled {
-            enableAdBlock()
-        }
-        
+
         return configuration
     }
-    
+
     func handleSearch(searchText: String) {
         guard let encodedSearch = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
               let url = URL(string: "https://duckduckgo.com/?q=\(encodedSearch)") else { return }
         urlToLoad = url
     }
-    
+
     func refresh() {
         webView?.reload()
         
         // Scroll to top after a short delay
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                    self.webView?.scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
-                }
+                       DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                           self.webView?.scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+                       }
     }
-    
+
     func goBack() {
         webView?.goBack()
+        
+        // Scroll to top after a short delay
+                       DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                           self.webView?.scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+                       }
     }
-    
+
     func goForward() {
         webView?.goForward()
+        
+        // Scroll to top after a short delay
+                       DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                           self.webView?.scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+                       }
     }
-    
+
     func setupWebView() {
         let configuration = self.configuration
         webView = WKWebView(frame: .zero, configuration: configuration)
     }
-    
+
     private func enableDNT() {
         let script = "navigator.doNotTrack = '1';"
         let userScript = WKUserScript(source: script, injectionTime: .atDocumentStart, forMainFrameOnly: true)
-        
+
         webView?.configuration.userContentController.addUserScript(userScript)
     }
-    
+
     private func disableDNT() {
         let script = "navigator.doNotTrack = '0';"
         let userScript = WKUserScript(source: script, injectionTime: .atDocumentStart, forMainFrameOnly: true)
-        
+
         webView?.configuration.userContentController.addUserScript(userScript)
     }
-    
+
     private func configureUserAgentSpoofing() {
         // Replace with the desired user agent string
         let userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        
+
         webView?.customUserAgent = userAgent
     }
-    
-    private func enableAdBlock() {
-        let adBlockScript = """
-        var style = document.createElement('style');
-        style.innerHTML = 'body > *:not(.ad) { display: none !important; }';
-        document.head.appendChild(style);
+
+    func toggleHyperBold() {
+        isHyperBoldEnabled.toggle()
+
+        let script = """
+        var elements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6, em, i, strong, b, li');
+        for (var i = 0; i < elements.length; i++) {
+            var words = elements[i].innerText.split(' ');
+            for (var j = 0; j < words.length; j++) {
+                var length = words[j].length;
+                if (length <= 2) {
+                    words[j] = '<b>' + words[j] + '</b>';
+                } else if (length <= 5) {
+                    words[j] = '<b>' + words[j].substring(0, 2) + '</b>' + words[j].substring(2);
+                } else if (length <= 7) {
+                    words[j] = '<b>' + words[j].substring(0, 3) + '</b>' + words[j].substring(3);
+                } else {
+                    words[j] = '<b>' + words[j].substring(0, 4) + '</b>' + words[j].substring(4);
+                }
+            }
+            elements[i].innerHTML = words.join(' ');
+        }
         """
-        
-        let userScript = WKUserScript(source: adBlockScript, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-        
-        webView?.configuration.userContentController.addUserScript(userScript)
+
+        let userScript = WKUserScript(source: script, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+
+        if isHyperBoldEnabled {
+            webView?.configuration.userContentController.addUserScript(userScript)
+        } else {
+            webView?.configuration.userContentController.removeAllUserScripts()
+        }
+        webView?.reload()
     }
-    
+
+
     // MARK: - Cookie Management
-    
+
     func getCookies(completion: @escaping ([HTTPCookie]) -> Void) {
         webView?.configuration.websiteDataStore.httpCookieStore.getAllCookies(completion)
     }
-    
+
     func deleteCookie(_ cookie: HTTPCookie) {
         webView?.configuration.websiteDataStore.httpCookieStore.delete(cookie)
     }
-    
+
     func deleteAllCookies() {
         webView?.configuration.websiteDataStore.httpCookieStore.getAllCookies { [weak self] cookies in
             cookies.forEach { cookie in
